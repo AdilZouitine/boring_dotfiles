319120afe139f89a63c33272e7df9a7a7eb5ee13
Adil Zouitine <adilzouitinegm@gmail.com>
2021-11-12 14:36:56 +0100

    add clang format

--- setup/install_editor.sh
+++ setup/install_editor.sh
@@ -1,5 +1,6 @@
 mkdir ~/.config/nvim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp -r nvim/* ~/.config/nvim/
+cp -rf clang/.clang-format ~/.clang-format
 nvim +'PlugInstall --sync' +qa
 
--- setup/update_editor.sh
+++ setup/update_editor.sh
@@ -3,5 +3,6 @@ NVIM_DIR="~/.config/nvim"
 if [ -d "$NVIM_DIR" ]; then
   echo "Installing config files in ${DIR}..."
   cp -rf -r nvim/* ~/.config/nvim/
+  cp -r clang/.clang-format ~/.clang-format
   nvim +'PlugInstall --sync' +qa
 fi
